7a56c4a712156a59e1615e1f444cc629ee22cf8e
Ethel Marquer <ethel.marquer@gmail.com>
2015-08-10 14:56:36 +0200

    First implementation of WebSocket

---
+++ socket.h
@@ -0,0 +1,33 @@
+#ifndef SOCKET
+#define SOCKET
+
+
+#include <QtCore/QObject>
+#include <QtCore/QList>
+#include <QtCore/QByteArray>
+
+class EchoServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit EchoServer(quint16 port, bool debug = false, QObject *parent = Q_NULLPTR);
+    ~EchoServer();
+
+Q_SIGNALS:
+    void closed();
+
+private Q_SLOTS:
+    void onNewConnection();
+    void processTextMessage(QString message);
+    void processBinaryMessage(QByteArray message);
+    void socketDisconnected();
+
+private:
+    QWebSocketServer *m_pWebSocketServer;
+    QList<QWebSocket *> m_clients;
+    bool m_debug;
+};
+
+
+#endif // SOCKET
+
